83cab9cc6119f0b2f4b75033ebb66538c6b47882
Mike Harrison <mike.harrison@joinroot.com>
2020-01-13 12:11:41 -0500

    Add note about flipping switch in slack command

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/api/slack', cors(corsOptions), (req, res) => {
         'type': 'section',
         'text': {
           'type': 'mrkdwn',
-          'text': 'http://whats-on-tap.nextwebtoday.com'
+          'text': "PS - Be a good Rootizen and flip the switch to off if you take the last of the keg!\nMore Info - http://whats-on-tap.nextwebtoday.com"
         }
       }
     ]
