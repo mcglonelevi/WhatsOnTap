7f19981e3fd40720a5a5e30ab1db75c297715029
Mike Harrison <mike.harrison@joinroot.com>
2020-01-13 11:50:29 -0500

    2 spaces indents / added learn more link to slack command

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-const port = 3000;
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 3000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 let beverageState = {
-    kombucha: false,
-    coldBrew: false,
-};
+  kombucha: false,
+  coldBrew: false,
+}
 
 const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 app.get('/api', cors(corsOptions), (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send(beverageState);
-});
+  res.set('Content-Type', 'application/json')
+  res.send(beverageState)
+})
 
 app.post('/api', cors(corsOptions), (req, res) => {
-    res.set('Content-Type', 'application/json');
-    beverageState = req.body;
-    res.send(beverageState);
-});
+  res.set('Content-Type', 'application/json')
+  beverageState = req.body
+  res.send(beverageState)
+})
 
 app.post('/api/slack', cors(corsOptions), (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send({
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": beverageState.kombucha? `There is kombucha :beerparrot:` : 'The kombucha is out :areyoukiddingme:',
-                },
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": beverageState.coldBrew? 'There is cold brew :celebrate:': 'The cold brew is out :sleepingparrot:',
-                }
-            }
-        ]
-    });
-});
+  res.set('Content-Type', 'application/json')
+  res.send({
+    'blocks': [
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': beverageState.kombucha ? `There is kombucha :beerparrot:` : 'The kombucha is out :areyoukiddingme:',
+        },
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': beverageState.coldBrew ? 'There is cold brew :celebrate:' : 'The cold brew is out :sleepingparrot:',
+        }
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '[Learn more](http://whats-on-tap.nextwebtoday.com)'
+        }
+      }
+    ]
+  })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
